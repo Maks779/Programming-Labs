113af533b19a4c743b4d09242b3c957dd89d681a
Maksym Bodnar <bodnarmaks9@gmail.com>
2026-04-17 18:29:30 +0200

    Add Lab8 files

---
+++ Lab8/Lab8Demo/ExtensionMethod/Program.cs
@@ -0,0 +1,54 @@
+﻿var someShittyString = "hello from the OTHer sIDe"; //-> "Hello from the other side"
+
+Console.WriteLine(someShittyString);
+
+var fixedString = someShittyString.ToSentence();
+
+Console.WriteLine(fixedString);
+
+Console.WriteLine(7.IsEven());
+Console.WriteLine(7.IsOdd());
+Console.WriteLine(2.IsPrime());
+
+List<int> numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+Console.WriteLine(numbers.First());
+Console.WriteLine(numbers.Second());
+
+public static class Extensions
+{
+    public static T Second<T>(this IEnumerable<T> input) => input.Skip(1).First();
+
+    public static string ToSentence(this string input)
+    {
+        return input[0].ToString().ToUpper() + input.Substring(1).ToLower();
+    }
+
+    public static bool IsEven(this int number) => number % 2 == 0;
+
+    public static bool IsOdd(this int number) => !number.IsEven();
+
+    public static bool IsPrime(this int number)
+    {
+        if (number == 1 )
+            return false;
+        if (number == 2)
+            return true;
+
+        for (int i = 2; i < number; i++)
+        {
+            if (number % i == 0)
+                return false;
+        }
+
+        return true;
+    }
+}
+
+//this idea would be cool if it worked :-(
+// public class BetterString : string
+// {
+//     public string ToSentence()
+//     {
+//         return ""; //something
+//     }
+// }
---
+++ Lab8/Lab8Demo/TuplesAndDeconstruction/Program.cs
@@ -0,0 +1,59 @@
+﻿//Tuples and Deconstruction
+
+var (gcd, lcm) = GcdLcm(48, 36);
+
+Console.WriteLine(gcd);
+Console.WriteLine(lcm);
+
+var s1 = new Student("James", "Bond", 45);
+
+var (name, age) = s1;
+Console.WriteLine(name);
+Console.WriteLine(age);
+
+(int Gcd, int Lcm) GcdLcm(int a, int b)
+{
+    var originalA = a;
+    var originalB = b;
+
+    while (a != b)
+    {
+        if (a > b)
+        {
+            a -= b;
+        }
+        else
+        {
+            b -= a;
+        }
+    }
+
+    return (a, originalA * originalB / a);
+}
+
+public class Student
+{
+    public string Name { get; set; }
+    public string LastName { get; set; }
+    public int Age { get; set; }
+
+    public Student(string name, string lastName, int age)
+    {
+        Name = name;
+        LastName = lastName;
+        Age = age;
+    }
+
+    //Deconstruction
+    public void Deconstruct(out string fullName, out int age)
+    {
+        fullName = $"{Name} {LastName}";
+        age = Age;
+    }
+}
+
+//public class Result
+//{
+//    public int Gcd { get; set; }
+//    public int Lcm { get; set; }
+//}
